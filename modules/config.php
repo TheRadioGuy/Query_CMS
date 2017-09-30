599e4cb41a0d3d5ba723cf244e207896e04af8d1
DuckerMan <sergeypechnikov326@gmail.com>
2017-09-30 19:41:40 +0300

    Add template settings in config

--- modules/config.php
+++ modules/config.php
@@ -2,5 +2,6 @@
 
 class Config{
 	public $database = array('login' => 'DATABASE_LOGIN', 'password' => 'PASSWORD_DATABASE', 'databaseName' => 'DATABASENAME', 'host' => 'HOSTNAME');
+	public $template = array('title' => "Дефолтная страница QueryCMS", 'templateName' => 'defaultTemplate1', 'logoCatalog' => 'images/logo.png');
 }
 ?>
