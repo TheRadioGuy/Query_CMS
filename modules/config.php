622689c281827f683ee1836027fe91a31c25a7fe
DuckerMan <sergeypechnikov326@gmail.com>
2017-09-30 19:55:19 +0300

    Re-write config x2

--- modules/config.php
+++ modules/config.php
@@ -1,7 +1,29 @@
 <?php
 
 class Config{
-	public $database = array('login' => 'DATABASE_LOGIN', 'password' => 'PASSWORD_DATABASE', 'databaseName' => 'DATABASENAME', 'host' => 'HOSTNAME');
-	public $template = array('title' => "Дефолтная страница QueryCMS", 'templateName' => 'defaultTemplate1', 'logoCatalog' => 'images/logo.png');
+	public function updateSettings($key, $value, $type){
+		if(empty($key) or empty($value) or empty($type)){
+			return array('errorDetail' => 'Paramets is empty', 'success' => false);
+		}
+
+		$rawSettings = file_get_contents('config.sl');
+		if(!empty($rawSettings)) $settings = unserialize($rawSettings);
+		else return array('errorDetail' => 'Config is empty', 'success' => false);
+		
+		$settings[$type][$key] = $value; // Write data
+
+		file_put_contents('config.sl', serialize($settings));
+
+		return array('success' => true);
+ 	}
+
+ 	function getSettings($key, $type){
+ 		if(empty($key) or empty($type)){
+ 			return array('errorDetail' => 'Paramets is empty', 'success' => false);
+ 		}
+
+ 		$settings = unserialize(file_get_contents('config.sl'));
+ 		return $settings[$type][$key];
+ 	}
 }
 ?>
