dc945e370f89e41cebf5531680b2c0250ba62bdd
DuckerMan <sergeypechnikov326@gmail.com>
2017-09-30 20:18:24 +0300

    Created Mysqli Class

---
+++ modules/Mysqli.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database{
+	public $authData;
+	public $connection;
+
+	function __construct($authData){
+		$this->authData = $authData;
+		/*
+ 	$authData = array('login' => 'root', 'password' => '123456', 'host' => 'localhost', 'database' => 'QueryCMS');
+
+
+		 */
+
+ 	$link = new mysqli($this->authData['host'], $this->authData['login'], $this->authData['password'], $this->authData['database']);
+ 	if ($link->connect_errno) {
+    echo "Не удалось подключиться к MySQL: " . $link->connect_error;
+}
+    $this->connection = $link;
+
+
+	}
+
+	public function queryDatabase($query){
+		return $this->connection->query($query);
+	}
+	public function realEscapeString($string){
+		return $this->connection->real_escape_string($string);
+	}
+
+	function __destruct(){
+		$this->connection->close();
+	}
+}
+?>
