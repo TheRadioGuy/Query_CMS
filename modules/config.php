a0a20e88a1a966e47386ab9b12340f61757af13b
DuckerMan <sergeypechnikov326@gmail.com>
2017-10-02 20:58:33 +0300

    Улучшил конфиг

--- modules/config.php
+++ modules/config.php
@@ -1,18 +1,20 @@
 <?php
 
 class Config{
+	private $config;
+	function __construct(){
+		$this->config = unserialize(file_get_contents('config.sl'));
+	}
 	public function updateSettings($key, $value, $type){
 		if(empty($key) or empty($value) or empty($type)){
 			return array('errorDetail' => 'Paramets is empty', 'success' => false);
 		}
 
-		$rawSettings = file_get_contents('config.sl');
-		if(!empty($rawSettings)) $settings = unserialize($rawSettings);
-		else return array('errorDetail' => 'Config is empty', 'success' => false);
 		
-		$settings[$type][$key] = $value; // Write data
+		
+		$this->config[$type][$key] = $value; // Write data
 
-		file_put_contents('config.sl', serialize($settings));
+		file_put_contents('config.sl', serialize($this->config));
 
 		return array('success' => true);
  	}
@@ -22,8 +24,8 @@ class Config{
  			return array('errorDetail' => 'Paramets is empty', 'success' => false);
  		}
 
- 		$settings = unserialize(file_get_contents('config.sl'));
- 		return $settings[$type][$key];
+ 		
+ 		return $this->config[$type][$key];
  	}
 }
 ?>
