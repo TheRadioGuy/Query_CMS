d074f5fa1a62fc8cf011819ea22324edbfa5b4ce
DuckerMan <sergeypechnikov326@gmail.com>
2017-10-15 16:31:00 +0300

    Base template download class

---
+++ modules/templateLoad.php
@@ -0,0 +1,28 @@
+<?php
+if(!defined(INCLUDE)) die('ooops...');
+
+
+
+
+
+class templateLoad{
+
+	private $templatesCatalog = '../assets/templates/';
+	private $tmpCatalog = '../core/tmp/';
+	private $downloadUrl = 'http://templates.querycms.ru/templates/';
+
+
+	private function move($tmpName){
+
+	}
+
+	private function download($tplName){
+
+	}
+
+	public function install($tplName){
+		
+	}
+
+}
+?>
